20fecb7f527fb73696600267ce26531dd1e5a298
Charpell <charpellumeh@gmail.com>
2018-04-11 15:56:10 +0100

    Create boilerplate for all 6 screens

---
+++ screens/DeckScreen.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+
+class DeckScreen extends Component {
+  render() {
+    <View>
+      <Text>DeckScreen</Text>
+      <Text>DeckScreen</Text>
+      <Text>DeckScreen</Text>
+      <Text>DeckScreen</Text>
+      <Text>DeckScreen</Text>
+      <Text>DeckScreen</Text>
+    </View>
+  }
+}
+
+export default DeckScreen;
