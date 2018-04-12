57f0295ff56d2011e0217e267119ac4685e7db53
Charpell <charpellumeh@gmail.com>
2018-04-12 05:27:31 +0100

    Styling the Navbar for different Styling

--- screens/ReviewScreen.js
+++ screens/ReviewScreen.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { View, Text } from "react-native";
+import { View, Text, Platform } from "react-native";
 import { Button } from 'react-native-elements';
 
 class ReviewScreen extends Component {
@@ -8,7 +8,16 @@ class ReviewScreen extends Component {
     
     return {
       title: 'Review Jobs',
-      headerRight: <Button title="Settings" onPress={() => navigate('settings')} />
+      headerStyle: {
+        marginTop: Platform.OS === 'android' ? 24 : 0
+      },
+      headerRight: 
+        <Button 
+          title="Settings"
+          onPress={() => navigate('settings')} 
+          backgroundColor="rgba(0,0,0,0)"
+          color="rgba(0, 122, 255, 1)"
+        />
     }
   }
 
