47f93803bf1c6de09a6da06af2768879c9147a7a
Charpell <charpellumeh@gmail.com>
2018-04-12 08:35:47 +0100

    Styleing the Welcome Screen Component

--- components/Slides.js
+++ components/Slides.js
@@ -1,13 +1,38 @@
 import React, { Component } from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, ScrollView } from "react-native";
 
 class Slides extends Component {
+  renderSlides() {
+    return this.props.data.map((slide) => {
+      return (
+        <View key={slide.text} style={styles.slideStyle}>
+          <Text styles={styles.textStyle}>{slide.text}</Text>
+        </View>
+      )
+    })
+  }
+
   render() {
     return (
-      <View>
-
-      </View>
+      <ScrollView
+        horizontal
+        style={{ flex: 1 }}
+      
+      >
+        {this.renderSlides()}
+      </ScrollView>
     );
+  }
+}
+
+const styles = {
+  slideStyle: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  slideText: {
+    fontSize: 30
   }
 }
 
--- screens/WelcomeScreen.js
+++ screens/WelcomeScreen.js
@@ -1,20 +1,32 @@
 import React, { Component } from "react";
-import { View, Text } from "react-native";
+import { StyleSheet, View, Text } from "react-native";
 import Slides from '../components/Slides';
 
 const SLIDE_DATA = [
   { text: 'Welcome to JobApp'},
+  { text: 'Use this to get a Job'},
   { text: 'Set your Location, then swipe away'},
 ]
 
 class WelcomeScreen extends Component {
   render() {
     return (
-      <View>
+      <View style={styles.container}>
+        
         <Slides data={SLIDE_DATA}/>
       </View>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center"
+  }
+});
+
 
 export default WelcomeScreen;
