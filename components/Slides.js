8026a61094c2fc7b8cca2910c85ec9db6db1d052
Charpell <charpellumeh@gmail.com>
2018-04-12 05:39:40 +0100

    Add the Slide Component to the Welcome Screen

---
+++ components/Slides.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import { StyleSheet, Text, View } from "react-native";
+
+class Slides extends Component {
+  render() {
+    return (
+      <View>
+
+      </View>
+    );
+  }
+}
+
+export default Slides;
