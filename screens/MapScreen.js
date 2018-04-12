5427636ec8f0135b313af77b04f6c02aa5c5d7d7
Charpell <charpellumeh@gmail.com>
2018-04-13 00:30:22 +0100

    Issuing Action Creators from MapScreen

--- screens/MapScreen.js
+++ screens/MapScreen.js
@@ -1,20 +1,33 @@
 import React, { Component } from "react";
 import { View, Text, ActivityIndicator } from "react-native";
-import { MapView } from 'expo'
+import { MapView } from 'expo';
+import { connect } from 'react-redux';
+import { Button } from 'react-native-elements';
+
+import * as actions from '../actions'
 
 class MapScreen extends Component {
   state = {
     mapLoaded: false,
     region: {
-      longitude: -122,
-      latitude: 37,
-      longitudeDelta: 0.04,
-      latitudeDelta: 0.09
+      latitude: 37.78825,
+      longitude: -122.4324,
+      latitudeDelta: 0.0922,
+      longitudeDelta: 0.0421,
     }
   }
 
   componentDidMount() {
     this.setState({ mapLoaded: true })
+  }
+
+  onRegionChangeComplete = (region) => {
+    console.log(region)
+    this.setState({ region })
+  }
+
+  onButtonPress = () => {
+    this.props.fetchJobs(this.state.region)
   }
 
   render() {
@@ -29,11 +42,32 @@ class MapScreen extends Component {
       <View style={{ flex: 1 }}>
         <MapView
           region={this.state.region} 
-          style={{ flex: 1 }} 
+          style={{ flex: 1 }}
+          onRegionChangeComplete={this.onRegionChangeComplete}           
         />
+        <View style={styles.buttonContainer}>
+          <Button 
+            large
+            title="Search This Area"
+            backgroundColor="#009688"
+            icon={{ name: 'search' }}
+            onPress={this.onButtonPress}
+          />
+        </View>
       </View>
     );
   }
 }
 
-export default MapScreen;
+const styles = {
+  buttonContainer: {
+    position: 'absolute',
+    bottom: 20,
+    left: 0,
+    right: 0
+  }
+}
+
+
+export default connect(null, actions)(MapScreen);
+
