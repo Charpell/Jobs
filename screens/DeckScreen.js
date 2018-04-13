6a60987e8a8c893d33c9f017ef07efb1338f63a1
Charpell <charpellumeh@gmail.com>
2018-04-13 01:19:36 +0100

    Importing the Deck Component

--- screens/DeckScreen.js
+++ screens/DeckScreen.js
@@ -1,18 +1,21 @@
 import React, { Component } from "react";
 import { View, Text } from "react-native";
 import { connect } from 'react-redux';
+import Swipe from '../components/Swipe'
 
 class DeckScreen extends Component {
   render() {
     return (
       <View>
-        
+        <Swipe 
+          data={this.props.jobs}
+        />
       </View>
     );
   }
 }
 
-function mapStateToProps({jobs }) {
+function mapStateToProps({ jobs }) {
   return { jobs: jobs.results }
 }
 
